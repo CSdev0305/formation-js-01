292e577e12f14b1fa934c9d456b0bae3ab768001
Claudin Saintil <claudinsaintil@gmail.com>
2024-04-04 11:48:28 -0400

    variable, boucle, structure cond...

--- index.js
+++ index.js
@@ -1,1 +1,83 @@
-console.log('Hello world');
+let my_const = 76;
+my_const = 43;
+
+let chaine = "Hello world";
+chane = "hello";
+chaine = `Hello ${my_const}`;
+let numero = 87.0;
+let open = true;
+let close = false;
+let nullValue = null;
+let undefinedValue = undefined;
+// console.log(typeof undefinedValue);
+
+/**
+ * - soustraction
+ * + addition
+ * * multiplication
+ * / division
+ * % modulo
+ * =
+ * >=
+ * <=
+ * ==
+ * ===
+ * !==
+ * !=
+ * ++
+ * --
+ * &&
+ * ||
+ * !
+ */
+
+// numero >= my_const ? console.log("Vrai") : console.log("Faux");
+
+// if (numero >= my_const) {
+//   console.log("Message");
+// }
+
+let tableNumber = [1, 3, 4, 6, "hello", "test"];
+// console.log(tableNumber)
+// cls
+// for(let i in tableNumber){
+//     console.log(tableNumber[i]);
+// }
+let identite = {
+  nom: "Saintil",
+  prenom: "Claudin",
+};
+for(let i in identite){
+    console.log(i);
+}
+// let i = 0;
+// while (i < tableNumber.length) {
+//   console.log(tableNumber[i]);
+//   i++;
+// }
+
+// do {
+//   console.log(tableNumber[i]);
+//   i++;
+// } while (i < tableNumber.length);
+
+// for (let i of tableNumber) {
+//   if (typeof i === "string") console.log(i);
+// }
+// let i = 0;
+// i = tableNumber[0];
+
+// if (numero >= my_const) console.log("Message");
+// let hello = "Salut"
+// if (numero) {
+//    let hello = "bonjour";
+//   console.log(hello)
+// } else {
+//     console.log(hello);
+// }
+// console.log(hello)
+// if (numero) {
+//   if (numero) {
+//   }
+// } else if (nullValue) {
+// }
